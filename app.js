67ab11a1fb2a7eaece115015bd5db999717a15c6
JASantia85 <JuanASantiago1058@gmail.com>
2022-08-22 11:52:43 -0400

    score decreases when guess is wrong

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 const secretNum = Math.trunc(Math.random() * 30) + 1
 document.querySelector('.number').textContent = secretNum
 
+let score = 30
+
+console.log(typeof score)
+
 document.querySelector('.check').addEventListener('click',
   function() {
     const guess = Number(document.querySelector('.guess').value)
@@ -11,5 +15,13 @@ document.querySelector('.check').addEventListener('click',
       document.querySelector('.message').textContent = 'Please Enter a Number...'
     } else if (guess === secretNum) {
       document.querySelector('.message').textContent = "🙌🏽 Correct Number!"
+    } else if (guess < secretNum) {
+      document.querySelector('.message').textContent = '⬇️ Too Low';
+      score--
+      document.querySelector('.score').textContent = score
+    } else if (guess > secretNum) {
+      document.querySelector('.message').textContent = '⬆️ Too High';
+      score--
+      document.querySelector('.score').textContent = score
     }
   })
